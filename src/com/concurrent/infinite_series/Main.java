bdf283729d84934257523dd9e78d33ee7cecc69f
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-07-25 15:53:15 -0300

    Initial file structure.

---
+++ src/com/concurrent/infinite_series/Main.java
@@ -0,0 +1,7 @@
+package com.concurrent.infinite_series;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Main works.");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module eulerInfiniteSeries {
+}
