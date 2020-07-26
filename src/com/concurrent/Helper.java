ede0b790c2d94b772b6ed892522062c2f672e04d
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2020-07-25 23:04:18 -0300

    Adding javadoc and updating class names.

--- src/com/concurrent/Helper.java
+++ src/com/concurrent/Helper.java
@@ -1,10 +1,27 @@
+/*
+ * @author Lucas Dantas
+ * @author Italo Epifanio
+ */
 package com.concurrent;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+/**
+ * Class Helper.
+ * This class provides two basic methods that are called throughout the project.
+ */
 public class Helper {
+	
+	/**
+	 * Handles with the user input.
+	 * Validates the argument provided by the user and returns the length of the 
+	 * series to be calculated.
+	 *
+	 * @param args 		User provided argument.
+	 * @return tasks	The series length (or how many tasks the system will create).
+	 */
 	public static int getSeriesLength(String[] args) {
 		if (args.length != 1) {
 			System.out.println("You must provide the series length as an argument.");
@@ -24,6 +41,12 @@ public class Helper {
 		return tasks;
 	}
 	
+	/**
+	 * Given a list of terms values, calculates and returns their sum.
+	 *
+	 * @param terms 	The list of terms.
+	 * @return sum		The sum of terms.
+	 */
 	public static Double calculateSeriesSum(List<Future<Double>> terms) {
 		double infinitSeriesSum = 0;
         
--- src/com/concurrent/Task.java
+++ src/com/concurrent/Task.java
@@ -1,23 +1,57 @@
+/*
+ * @author Ítalo Epifânio
+ * @author Lucas Dantas
+ */
 package com.concurrent;
 
 import java.util.concurrent.Callable;
 
+/**
+ * The Class Task.
+ * This class represents the task that will calculate the factorial of a given number.
+ */
 public class Task implements Callable<Double> {
+	
+	/** The term (i.e. the number to calculate its factorial). */
 	private int term;
 	
+	/**
+	 * Instantiates a new task.
+	 *
+	 * @param term The term to calculate factorial.
+	 */
 	public Task(int term) {
 		this.term = term;
 	}
 	
+	/**
+	 * Call.
+	 * Invokes the method responsible for calculating this infinite series term.
+	 *
+	 * @return The term, calculated.
+	 * @throws Exception.
+	 */
 	@Override
 	public Double call() throws Exception {
-		return this.infinitSeriesTermCalculator(this.term);
+		return this.infiniteSeriesTermCalculator(this.term);
 	}
 	
-	private double infinitSeriesTermCalculator(int term) {
+	/**
+	 * Calls the factorial of given number and returns it as a series term.
+	 *
+	 * @param term 			The value to have its factorial calculated.
+	 * @return seriesTerm	Returns the value calculated as a series term.	
+	 */
+	private double infiniteSeriesTermCalculator(int term) {
 		return 1.0 / factorial(term);
 	}
 	
+	/**
+	 * Calculates the factorial of a given number.
+	 *
+	 * @param n 			The number to calculate its factorial.
+	 * @return factorial	The factorial of given number.
+	 */
 	private int factorial(int n) {
 		System.out.println("Thread " + Thread.currentThread().getId() + " is calculating factorial of " + n);
 		
